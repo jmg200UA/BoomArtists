19af5ef3215f1d4c190c56c0d26e9dccbe0f5ad1
nazarbk <nazarblancokataran@gmail.com>
2023-07-12 02:17:34 +0200

    manu barra e intento de las peticiones en services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LastFmService } from './services/http.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  artistTracks: any[] = [];
+
+  constructor(private lastFmService: LastFmService) {}
+
+  ngOnInit(){
+    this.getArtistTracks();
+  }
+
+  async getArtistTracks(){
+    try{
+      const artistName = '';
+      const response = await this.lastFmService.getArtistTracks(artistName);
+      this.artistTracks = response.toptracks.track;
+    }catch (error){
+      console.error('Error al obtener las canciones del kismiz', error);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HttpService } from './services/http.service';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },HttpService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
