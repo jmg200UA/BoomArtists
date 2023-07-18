9b53deb86cc30018503f923f8ab138efd8c6d2c5
Javier Martín Gómez <j.martingomez@edu.gva.es>
2023-07-18 22:26:49 +0200

    tailwind, menu nuevo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  constructor() {}
 
   ngOnInit(){
-    
+    this.router.events.subscribe((event) => {
+      if (event instanceof RouterEvent) {
+        this.currentPage = event.url;
+      }
+    });
+    console.log("Pages: ", this.pages);
+  }
+
+  public pages = [
+    { title: 'Inicio', url: '/home' },
+    { title: 'Día 1', url: '/dia1' },
+    { title: 'Día 2', url: '/dia2' },
+    { title: 'Día 3', url: '/dia3' }
+  ];
+  public currentPage = '';
+
+  constructor(private router: Router) {
+
   }
 }
